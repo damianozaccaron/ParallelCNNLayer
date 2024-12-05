9387df2438aac2ed1ab5725940e165297e163b80
Sanzio <raffaele.sinani@gmail.com>
2024-12-05 14:29:22 +0100

    Update README.md

---
+++ src/random_matrix.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <stdlib.h>
+
+double ** random_matrix (int rows, int columns)
+{   
+    int i, j;
+    double ** arr = (double **)malloc(rows * sizeof(double *));
+    for (i = 0; i<rows; i++)
+    {
+        arr[i] = (double *)malloc(columns * sizeof(double));
+    }
+
+    srand(time(0));
+
+    for (i = 0; i < rows; i++)
+
+    {
+        for (j = 0; j < columns; j++)
+        {
+            arr[i][j] = rand() % 10;
+        }
+    }
+    return arr;
+}
+
+double * flatten_matrix (double ** matrix,int rows,int columns)
+{
+    double * result = (double *)malloc(rows*sizeof(double));
+    int i, j;
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < columns; j++) {
+            result[i * columns + j] = matrix[i][j];
+        }
+    }
+    return result;
+
+}
+
+void free_matrix (double ** matrix, int rows)
+{
+    int i;
+    for (i = 0; i < rows; i++)
+    {
+        free(matrix[i]);
+    }
+    free(matrix);
+}
+
+void print_matrix(double ** matrix, int rows, int columns) {
+    int x, y;
+
+    for(x = 0 ; x < rows ; x++) {
+        printf(" (");
+        for(y = 0 ; y < columns ; y++){
+            printf("%f     ", matrix[x][y]);
+        }
+        printf(")\n");
+    }
+}
---
+++ src/sequential.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <math.h>
+
+#include "random_matrix.c"
+
+#define NROWS 5
+#define NCOLS 5
+#define KER_SIZE 3
+
+
+int main()
+{
+    double ** my_matrix = random_matrix(NROWS, NCOLS);
+    double ** kernel = random_matrix(KER_SIZE, KER_SIZE);
+    int i,j, k, l, x, y;
+    double ** result =(double **)malloc((NROWS - (KER_SIZE - 1)) * sizeof(double *));
+    for (x = 0; x < NROWS - (KER_SIZE - 1); x++) {
+        result[x] = malloc((NCOLS - (KER_SIZE - 1)) * sizeof(double));
+        for (y = 0; y < NCOLS - (KER_SIZE - 1); y++) {
+            result[x][y] = 0.0; // Initialize each element to 0
+        }
+    }
+    print_matrix(my_matrix, NROWS, NCOLS);
+    printf("\n");
+    print_matrix(kernel, KER_SIZE, KER_SIZE);
+    printf("\n");
+    print_matrix(result, NROWS - (KER_SIZE - 1) ,(NCOLS - (KER_SIZE - 1)));
+    int row_idx, col_idx; //Not necessary, let's put it for now
+    /* Not really necessary in the sequential version
+    double * flattened_matrix = flatten_matrix(my_matrix, NROWS, NCOLS);
+    */
+
+    for (i = 0; i < NROWS; i++) //Iterates through the rows of the original matrix
+    {
+        for (j = 0; j < NCOLS; j++) //Iterates through the columns
+        {
+            for (k = 0; k < KER_SIZE; k++) //Iterates through the rows of the kernel
+            {
+                for (l = 0; l < KER_SIZE; l++) //Iterates through the columns of the kernel
+                {
+                    row_idx = i - (k - 1); //This is because the first row of the kernel only adds to the second line (so -(-1) = +1)
+                    col_idx = j - (l - 1);
+                    printf("The first indexes are %d, %d\n", row_idx, col_idx);
+                    if (col_idx >= 0 && col_idx < NCOLS - (KER_SIZE-1) && row_idx >= 0 && row_idx < NROWS - (KER_SIZE-1))
+                    {
+                        result[row_idx][col_idx] += my_matrix[i][j] * kernel[k][l];
+                        printf("\n");
+                        print_matrix(result, NROWS - (KER_SIZE - 1) ,(NCOLS - (KER_SIZE - 1)));
+                        printf("\n");
+                    }
+                }
+            }
+        }
+    }
+
+    
+    free_matrix(my_matrix, NROWS);
+    free_matrix(kernel, NROWS);
+    free_matrix(result, NROWS - (KER_SIZE - 1));
+
+    return 0;
+}
